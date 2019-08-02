6f754d2f2dfad915273a33f571dac0112bc7a587
LeoSL <leosilvalima@gmail.com>
2019-08-02 10:36:38 -0400

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-unused-expressions */
+import { createStore } from "redux";
+import { connect } from "react-redux";
+import React from "react";
+import "./App.css";
+const RECORD_ACTION = "RECORD_ACTION";
+const setupStore = {
+  actions: [
+    {
+      user: "Xunder",
+      action: "Did something"
+    }
+  ]
+};
 
-function App() {
+const reducer = (action, initialState) => {
+  if (typeof state === "undefined") {
+    return initialState;
+  }
+
+  switch (action) {
+    case RECORD_ACTION:
+      return {
+        actions: [
+          ...initialState.actions,
+          {
+            user: action.user,
+            actionText: action.text
+          }
+        ]
+      };
+    default:
+      return initialState;
+  }
+};
+
+const store = createStore(reducer, setupStore);
+
+const ActionsPresenter = actions => {
+  console.log(actions);
+
+  return (
+    actions && (
+      <ul>
+        {actions.map((action, index) => {
+          <li key={index}>{`${action.user} DID => ${action.text}`}</li>;
+        })}
+      </ul>
+    )
+  );
+};
+
+const App = () => {
+  store.subscribe(ActionsPresenter);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>😎 Redux Actions Spy 😎</h1>
       </header>
+      <ActionsPresenter actions={this.props.actions} />
     </div>
   );
-}
+};
 
-export default App;
+const AppContainer = connect(function mapStateToProps(state) {
+  return {
+    actions: state.actions
+  };
+})(App);
+
+const Provider = () => (
+  <Provider store={store}>
+    <AppContainer />
+  </Provider>
+);
+
+export default Provider;
